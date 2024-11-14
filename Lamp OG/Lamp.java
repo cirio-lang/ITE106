1517cfd9eca93ed4238c5222bf036af04d04bd83
cirio-lang <ciriojefferson983@gmail.com>
2024-11-14 15:45:53 +0800

    Create Lamp.java

---
+++ Lamp OG/Lamp.java
@@ -0,0 +1,13 @@
+class Lamp {
+  
+  boolean isOn;
+
+  void turnOn() {
+    isOn = true;
+    System.out.println("Light on? " + isOn);
+  }
+  void turnOff() {
+    isOn = false;
+    System.out.println("Light on? " + isOn);
+  }
+}
